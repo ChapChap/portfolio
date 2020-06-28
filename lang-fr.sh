76a12d93dbd0293dff9a5fdecac00e5d20615359
Chap <thechap@pm.me>
2020-06-28 23:54:41 +0200

    fix EN vers FR

--- lang-fr.sh
+++ lang-fr.sh
@@ -1,8 +1,11 @@
 #/usr/bin/env bash
 
-sed -i '' 's/Hi, I am/Bonjour, je suis/' themes/toha/layouts/partials/home.html
-sed -i '' 's/ at / chez /' themes/toha/layouts/partials/about.html
-sed -i '' 's/>My Resume</>Mon CV</' themes/toha/layouts/partials/about.html
-sed -i '' 's/>Experiences</>Expériences</' themes/toha/layouts/partials/experiences.html
-sed -i '' 's/>Projects</>Projets</' themes/toha/layouts/partials/projects.html
-sed -i '' 's/>Skills</>Compétences</' themes/toha/layouts/partials/skills.html
+mkdir -p layouts/partials
+touch layouts/partials/home.html layouts/partials/about.html \
+layouts/partials/experiences.html layouts/partials/projects.html layouts/partials/skills.html
+
+sed 's/Hi, I am/Bonjour, je suis/' themes/toha/layouts/partials/home.html > layouts/partials/home.html
+sed 's/ at / chez /;s/>My Resume</>Mon CV</' themes/toha/layouts/partials/about.html > layouts/partials/about.html
+sed 's/>Experiences</>Expériences</' themes/toha/layouts/partials/experiences.html > layouts/partials/experiences.html
+sed 's/>Projects</>Projets</' themes/toha/layouts/partials/projects.html > layouts/partials/projects.html
+sed 's/>Skills</>Compétences</' themes/toha/layouts/partials/skills.html > layouts/partials/skills.html
